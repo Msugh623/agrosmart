aebcc77408e26f996ad4a6eb85a5dee2a5c7f4f7
Chia Ernest <iternenge469@gmail.com>
2024-10-05 20:00:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'react-lazy-load-image-component/src/effects/opacity.css';
-import './css/index.css';
 import './css/bootstrap.css';
 import './css/toastify.css'
 import './css/animate.css';
+import './css/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
